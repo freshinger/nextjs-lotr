3a689ad6dfc18745deb64981eca3f716b693b661
Daniel <daniel_ciecior@yahoo.de>
2025-08-13 11:19:27 +0200

    initial commit

---
+++ src/app/volumes/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+import { volumes } from "../../../lib/data";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  const book = volumes.filter(volume => volume.slug === slug)[0];
+  const index = volumes.indexOf(book);
+  const books = book.books.map( (book, index) => 
+  <li key={index}>
+    <h3>{book.ordinal} {book.title}</h3>
+  </li>)
+  let nextButtons = <div></div>;
+  switch (index) {
+    case 0:
+      nextButtons = <div><Link href={volumes[index+1].slug}>Next</Link></div>;
+      break;
+    case volumes.length-1:
+      nextButtons = <div><Link href={volumes[index-1].slug}>Previous</Link></div>;
+      break;
+    default:
+      nextButtons = <div>
+        <Link href={volumes[index-1].slug}>Previous</Link>
+        <Link href={volumes[index+1].slug}>Next</Link>
+        </div>;
+      break;
+  }
+  return (
+    <div>
+      <Link href="/volumes">All Volumes</Link>
+      <h1>{book.title}</h1>
+      <p>{book.description}</p>
+      <ul>{books}</ul>
+      <Image src={book.cover} alt={book.cover} width='140' height='230'></Image>
+      <hr></hr>
+      {nextButtons}
+    </div>
+  )
+}
---
+++ src/app/volumes/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { introduction, volumes } from "../../lib/data";
+
+export default function Page() {
+  const volumeItems = volumes.map( (volume, index) => 
+  <li key={index}><Link href={'/volumes/'+volume.slug}>{volume.title}</Link></li>);
+
+  const randomIndex = Math.floor(Math.random() * volumes.length);
+
+  return (
+    <main>
+        <h1>Lord of the Rings</h1>
+        <p>{introduction}</p>
+        <h2>All Volumes</h2>
+        <hr></hr>
+        <ul>
+          {volumeItems}
+        </ul>
+        <hr></hr>
+        <Link href={'/volumes/'+volumes[randomIndex].slug}>Random Volume</Link>
+    </main>
+  );
+}
